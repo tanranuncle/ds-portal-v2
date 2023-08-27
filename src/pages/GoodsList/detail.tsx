701efb7427df9d215902321202836f942dba9769
wuyuanchao <fourier@jollycorp.com>
2023-08-27 12:28:26 +0800

    feat(goods): 详情页改版

--- src/pages/GoodsList/detail.tsx
+++ src/pages/GoodsList/detail.tsx
@@ -7,8 +7,8 @@ import {
   tagEnumMap,
 } from '@/services/apis/goods';
 import { DeleteOutlined, EditOutlined, EuroCircleOutlined } from '@ant-design/icons';
-import { GridContent, PageContainer, ProDescriptions } from '@ant-design/pro-components';
-import { Card, Col, Divider, Empty, Image, message, Popconfirm, Row, Tag, Tooltip } from 'antd';
+import { PageContainer, ProDescriptions } from '@ant-design/pro-components';
+import { Button, Card, Empty, Image, message, Popconfirm, Tag } from 'antd';
 import React, { FC, useEffect, useState } from 'react';
 import { useParams } from 'umi';
 
@@ -21,7 +21,7 @@ import CommentTable from './component/commentTable';
 import UpdateForm from './component/eum';
 import SkuTable from './component/skuTable';
 
-export type tabKeyType = 'skuList' | 'records' | 'channels';
+export type tabKeyType = 'detail' | 'skuList' | 'records' | 'channels';
 
 const GoodsImgPreview: React.FC<{ imageList: string[] | undefined; width?: string | number }> = ({
   imageList,
@@ -54,7 +54,7 @@ const GoodsImgPreview: React.FC<{ imageList: string[] | undefined; width?: strin
 
 const DetailPage: FC = () => {
   const [current, setCurrent] = useState<Partial<API.Goods> | undefined>(undefined);
-  const [tabKey, setTabKey] = useState<tabKeyType>('skuList');
+  const [tabKey, setTabKey] = useState<tabKeyType>('detail');
 
   const params = useParams();
 
@@ -69,6 +69,17 @@ const DetailPage: FC = () => {
 
   // 渲染tab切换
   const renderChildrenByTabKey = (tabValue: tabKeyType) => {
+    if (tabValue === 'detail') {
+      return (
+        <Card>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `${current?.remark.replaceAll('\n', '</br>')}`,
+            }}
+          ></div>
+        </Card>
+      );
+    }
     if (tabValue === 'skuList') {
       return <SkuTable loadData={loadData} current={current} />;
     }
@@ -79,7 +90,11 @@ const DetailPage: FC = () => {
       return <CommentTable goodsId={current?.goodsId} />;
     }
     if (tabValue === 'quote') {
-      return <QuoteTimeline goodsId={current?.goodsId} goodsSn={current?.goodsSn} />;
+      return (
+        <Card>
+          <QuoteTimeline goodsId={current?.goodsId} goodsSn={current?.goodsSn} />
+        </Card>
+      );
     }
     return null;
   };
@@ -89,108 +104,92 @@ const DetailPage: FC = () => {
   }
 
   return (
-    <PageContainer title={current?.goodsSn}>
-      <GridContent>
-        <Row gutter={24}>
-          <Col lg={7} md={24}>
-            <Card
-              cover={
-                <GoodsRibbon goodsType={current?.goodsType}>
-                  <GoodsImgPreview imageList={current?.imageUrls} />
-                </GoodsRibbon>
-              }
-              actions={[
-                <UpdateForm
-                  values={current}
-                  onFinish={async (value) => {
-                    const success = await editGoods(value as API.Goods);
-                    if (success) {
-                      loadData();
-                      return true;
-                    }
-                    return false;
-                  }}
-                  trigger={<EditOutlined key="edit" />}
-                  title="编辑商品"
-                />,
-
-                <Popconfirm
-                  title="删除商品"
-                  description="确认要删除这个商品吗?"
-                  onConfirm={async () => {
-                    const success = await deleteGoods({ goodsId: current?.goodsId } as API.Goods);
-                    if (success) {
-                      message.success('删除成功');
-                      //window.history.back();
-                      loadData();
-                    }
-                    return true;
-                  }}
-                  okText="确认"
-                  cancelText="取消"
-                  key="delete"
-                >
-                  <DeleteOutlined />
-                </Popconfirm>,
-                <Tooltip title="报价">
-                  <EuroCircleOutlined
-                    key="quote"
-                    onClick={() => {
-                      window.open('/goodsQuote/' + current?.goodsSn, '_blank');
-                    }}
-                  />
-                </Tooltip>,
-              ]}
-            >
-              <Card.Meta
-                title={current?.goodsName}
-                description={
-                  <>
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: `${current?.remark.replaceAll('\n', '</br>')}`,
-                      }}
-                      style={{ height: '100px', overflow: 'auto' }}
-                    ></div>
-                    <Divider dashed />
-                    <ProDescriptions column={1}>
-                      <ProDescriptions.Item label="收货仓库" valueEnum={depotEnum}>
-                        {current?.depot}
-                      </ProDescriptions.Item>
-                      <ProDescriptions.Item label="库存状态" valueEnum={tagEnumMap}>
-                        {current?.availability}
-                      </ProDescriptions.Item>
-                    </ProDescriptions>
-                    {current?.goodsTags?.map((x) => (
-                      <Tag key={current?.goodsId + '_' + x} color={x}>
-                        {x}
-                      </Tag>
-                    ))}
-                    {/* <TagList tags={[{ key: 'red', label: 'red' }]} /> */}
-                  </>
-                }
-              />
-            </Card>
-          </Col>
-          <Col lg={17} md={24}>
-            <Card
-              bordered={false}
-              tabList={[
-                { key: 'skuList', tab: 'sku列表 ' },
-                { key: 'channels', tab: '运输线路' },
-                { key: 'quote', tab: '报价历史' },
-                { key: 'records', tab: '记录' },
-              ]}
-              activeTabKey={tabKey}
-              onTabChange={(_tabKey: string) => {
-                setTabKey(_tabKey as tabKeyType);
-              }}
-            >
-              {renderChildrenByTabKey(tabKey)}
-            </Card>
-          </Col>
-        </Row>
-      </GridContent>
+    <PageContainer
+      title={current?.goodsSn}
+      extra={[
+        <Button
+          type="primary"
+          icon={<EuroCircleOutlined />}
+          key="quote"
+          onClick={() => {
+            window.open('/goodsQuote/' + current?.goodsSn, '_blank');
+          }}
+        >
+          报价
+        </Button>,
+        <UpdateForm
+          key="edit"
+          values={current}
+          onFinish={async (value) => {
+            const success = await editGoods(value as API.Goods);
+            if (success) {
+              loadData();
+              return true;
+            }
+            return false;
+          }}
+          trigger={<Button icon={<EditOutlined />}>编辑</Button>}
+          title="编辑商品"
+        />,
+        <Popconfirm
+          title="删除商品"
+          description="确认要删除这个商品吗?"
+          onConfirm={async () => {
+            const success = await deleteGoods({ goodsId: current?.goodsId } as API.Goods);
+            if (success) {
+              message.success('删除成功');
+              //window.history.back();
+              loadData();
+            }
+            return true;
+          }}
+          okText="确认"
+          cancelText="取消"
+          key="delete"
+        >
+          <Button icon={<DeleteOutlined />}>删除</Button>
+        </Popconfirm>,
+      ]}
+      content={
+        <>
+          <ProDescriptions column={1}>
+            <ProDescriptions.Item label="商品名称">{current?.goodsName}</ProDescriptions.Item>
+            <ProDescriptions.Item label="收货仓库" valueEnum={depotEnum}>
+              {current?.depot}
+            </ProDescriptions.Item>
+            <ProDescriptions.Item label="库存状态" valueEnum={tagEnumMap}>
+              {current?.availability}
+            </ProDescriptions.Item>
+          </ProDescriptions>
+          {current?.goodsTags?.map((x) => (
+            <Tag key={current?.goodsId + '_' + x} color={x}>
+              {x}
+            </Tag>
+          ))}
+          {/* <TagList tags={[{ key: 'red', label: 'red' }]} /> */}
+          {/*<Divider dashed />*/}
+        </>
+      }
+      extraContent={
+        <div style={{ width: '200px' }}>
+          <GoodsRibbon goodsType={current?.goodsType}>
+            <GoodsImgPreview imageList={current?.imageUrls} />
+          </GoodsRibbon>
+        </div>
+      }
+      tabList={[
+        { key: 'detail', tab: '详情' },
+        { key: 'skuList', tab: 'sku列表 ' },
+        { key: 'channels', tab: '运输线路' },
+        { key: 'quote', tab: '报价历史' },
+        { key: 'records', tab: '记录' },
+      ]}
+      tabActiveKey={tabKey}
+      onTabChange={(_tabKey: string) => {
+        setTabKey(_tabKey as tabKeyType);
+      }}
+    >
+      {renderChildrenByTabKey(tabKey)}
     </PageContainer>
   );
 };
